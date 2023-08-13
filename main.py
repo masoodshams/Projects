8d964275dc9797dc8119fd4ca4a9d460f36e349d
masoodshams <masoodshamsnia@gmail.com>
2023-08-13 22:18:51 +0330

    adding comments

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from sklearn import datasets         # To create dummy dataset
-from sklearn.cluster import KMeans
-from sklearn_extra.cluster import KMedoids
-from sklearn.mixture import GaussianMixture
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import DBSCAN
+from sklearn import datasets         # برای ساخت دیتاست ساختگی
+from sklearn.cluster import KMeans  # اضافه کردن K-Means
+from sklearn_extra.cluster import KMedoids  # اضافه کردن KMedoids
+from sklearn.mixture import GaussianMixture  # اضافه کردن GaussianMixture
+from sklearn.cluster import AgglomerativeClustering  # اضافه کردن AgglomerativeClustering
+from sklearn.cluster import DBSCAN  # اضافه کردن DBSCAN
 from sklearn.preprocessing import StandardScaler
 
 import numpy as np
@@ -11,13 +11,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
-warnings.filterwarnings('ignore')    # To get rid of warning messages
+warnings.filterwarnings('ignore')    # برای حذف کردن وارنینگ ها
 
 
 # Remove scientific notations and display numbers with 2 decimal points instead
+# حذف نمایش علمی و نمایش اعداد با 2 رقم صحیح
 pd.options.display.float_format = '{:,.2f}'.format
 
 # Update default background style of plots
+# به روز کردن پس زمینه ی نمودارها
 sns.set_style(style='darkgrid')
 
 np.random.seed(1)   # Setting the seed to get reproducible results
@@ -30,13 +32,16 @@ df.columns = ['X1', 'X2']
 
 df['Y'] = y
 df
+
 # Scatter plot of original lables
+# نمودار پراکندگی برچسب های اصلی
 sns.scatterplot(x='X1', y='X2', data=df, hue='Y')
 
 plt.show()
 
-X = StandardScaler().fit_transform(X)
+X = StandardScaler().fit_transform(X)  # اسکیل کردن داده ها
 
+# K-Means
 kmeans = KMeans(n_clusters=2, random_state=12)
 
 kmeans.fit(X)
@@ -46,3 +51,5 @@ df['KmeansLabels'] = kmeans.predict(X)
 sns.scatterplot(x='X1', y='X2', data=df, hue='KmeansLabels')
 
 plt.show()
+
+#
